c6a707c6162d0bc3567fe1c485c5b637d55babef
vinaykarunakaran <vk6121997@gmail.com>
2024-10-21 16:14:46 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  useState,
+  useRef
+} from "react"; 
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+function App() { 
+  const inputRef = useRef(null); 
+  const resultRef = useRef(null); 
+  const [result, setResult] = useState(0); 
+ 
+  function plus(e) { 
+    e.preventDefault(); 
+    setResult((result) => result + Number(inputRef.current.value)); 
+    inputRef.current.focus();
+    inputRef.current.select();
+  }; 
+ 
+  function minus(e) { 
+  	e.preventDefault(); 
+    setResult((result) => result - Number(inputRef.current.value));
+    inputRef.current.focus();
+    inputRef.current.select();
+  };
+ 
+  function times(e) { 
+    e.preventDefault(); 
+    setResult((result) => result * Number(inputRef.current.value));
+    inputRef.current.focus();
+    inputRef.current.select();
+  }; 
+ 
+  function divide(e) { 
+    e.preventDefault(); 
+    setResult((result) => result / Number(inputRef.current.value));
+    inputRef.current.focus();
+    inputRef.current.select();
+  };
+ 
+  function resetInput(e) {
+    e.preventDefault();
+    inputRef.current.value=0; 
+    inputRef.current.focus();
+    inputRef.current.select();
+  }; 
+ 
+  function resetResult(e) { 
+    e.preventDefault();
+  	setResult(0);
+    inputRef.current.focus();
+    inputRef.current.select();
+  }; 
+ 
+  return ( 
+    <div className="App"> 
+      <div> 
+        <h1>Simplest Working Calculator</h1> 
+      </div> 
+      <form> 
+        <p ref={resultRef}> 
+          Result : {result} 
+        </p> 
+        <input
+          pattern="[0-9]" 
+          ref={inputRef} 
+          type="number" 
+          placeholder="Type a number" 
+        /> 
+        <button onClick={plus}>Add</button> 
+        <button onClick={minus}>Subtract</button> 
+        <button onClick={times}>Multiply</button> 
+        <button onClick={divide}>Divide</button> 
+        <button onClick={resetInput}>Reset Input</button> 
+        <button onClick={resetResult}>Reset result</button>
+      </form> 
+    </div> 
+  ); 
+} 
+ 
+export default App; 
